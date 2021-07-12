4d7b6861efb5589853619341b2c7a19ba0d35dbd
AhmedYousof <31749142+AhmedYousof@users.noreply.github.com>
2021-07-12 20:13:02 +0200

    Add files via upload

---
+++ client/src/component/auth/LearnerRegister.js
@@ -0,0 +1,127 @@
+import React, { Component } from "react";
+import axios from "axios";
+import classnames from "classnames";
+import { Link } from "react-router-dom";
+
+class LearnerRegister extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {},
+      email: "",
+      name: "",
+      password: "",
+      confirmPassword: "",
+      isAuthenticated: false,
+      errors: {},
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  onSubmit(e) {
+    e.preventDefault();
+    const userData = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      confirmpassword: this.state.confirmPassword,
+    };
+    axios
+      .post("/api/learner/register", userData)
+      .then((res) => {
+        const user = res.data;
+        this.setState({ user });
+        window.location.assign("/login");
+      })
+      .catch((err) => {
+        if (err.response) this.setState({ errors: err.response.data });
+      });
+  }
+
+  render() {
+    const { errors } = this.state;
+    return (
+      <div className="p-3">
+        <h2 className="m-5 text-info">Register As a Learner</h2>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group p-1">
+            <input
+              type="email"
+              onChange={this.onChange}
+              className={classnames("form-control", {
+                "is-invalid": errors.email,
+              })}
+              placeholder="Enter your email...."
+              name="email"
+              value={this.state.email}
+            />
+            {errors.email && (
+              <div className="invalid-feedback">{errors.email}</div>
+            )}
+          </div>
+          <div className="form-group p-1">
+            <input
+              type="text"
+              onChange={this.onChange}
+              className={classnames("form-control", {
+                "is-invalid": errors.name,
+              })}
+              placeholder="Enter your name...."
+              name="name"
+              value={this.state.name}
+            />
+            {errors.name && (
+              <div className="invalid-feedback">{errors.name}</div>
+            )}
+          </div>
+          <div className="form-group p-1">
+            <input
+              type="password"
+              onChange={this.onChange}
+              className={classnames("form-control form-control-lg", {
+                "is-invalid": errors.password,
+              })}
+              placeholder="Enter your password...."
+              name="password"
+              value={this.state.password}
+            />
+            {errors.password && (
+              <div className="invalid-feedback">{errors.password}</div>
+            )}
+          </div>
+          <div className="form-group p-1">
+            <input
+              type="password"
+              onChange={this.onChange}
+              className={classnames("form-control form-control-lg", {
+                "is-invalid": errors.confirmpassword,
+              })}
+              placeholder="Confirm your password...."
+              name="confirmPassword"
+              value={this.state.confirmPassword}
+            />
+            {errors.confirmpassword && (
+              <div className="invalid-feedback">{errors.confirmpassword}</div>
+            )}
+          </div>
+          <input
+            type="submit"
+            className="btn btn-info btn-block mt-4"
+            value="Register now"
+          />
+          <div className="pt-4 ">
+            <Link to="/register" className="btn  btn-link">
+              Register as an instructor instead!..
+            </Link>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LearnerRegister;
